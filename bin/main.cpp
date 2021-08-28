a31b2af53d46aeb99ed3c9e9c270a1f1dd337aad
Tessa Power <tessa@tessapower.co>
2021-08-29 11:24:22 +1200

    Map char to string

--- bin/main.cpp
+++ bin/main.cpp
@@ -4,16 +4,16 @@
 #include <iostream>
 #include <map>
 
-static std::map<char, char> m {
-    {'a', 'z'},
-    {'b', 'y'},
-    {'c', 'x'},
+static std::map<char, std::string> m {
+    {'a', "&a;"},
+    {'b', "&b;"},
+    {'c', "&c;"},
 };
 
 int main(const int argc, const char *argv[]) {
     const char* first_arg = argv[1];
     const char c = *first_arg;
-    
+
     std::cout << m[c] << std::endl;
 
     return EXIT_SUCCESS;
