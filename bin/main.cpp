d1f9b28750806b93f1bd033efe884a8d3813114d
Tessa Power <tessa@tessapower.co>
2021-08-29 10:33:00 +1200

    Print out char received from stdin

---
+++ bin/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Tessa Power on 29/08/21.
+//
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    char c = *argv[1];
+    std::cout << c << std::endl;
+
+    return EXIT_SUCCESS;
+}
