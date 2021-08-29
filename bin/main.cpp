8880938a34401563b064ab365c9c6558ae642e54
Tessa Power <tessa@tessapower.co>
2021-08-30 08:10:08 +1200

    WIP

--- bin/main.cpp
+++ bin/main.cpp
@@ -4,28 +4,31 @@
 #include <iostream>
 #include <map>
 
-static std::map<char, std::string> m {
-    {'<', "&lt;"},
-    {'>', "&gt;"},
-    {'&', "&amp;"},
-    {'\'', "&apos;"},
-    {'\"', "&quot;"},
-};
+#include "../esc_codes.cpp"
+
+int num_utf8_continuation_bytes(std::string const& str);
 
 int main(const int argc, const char *argv[]) {
     const char* first_arg = argv[1];
     std::string esc_string = "";
 
-    for (char c = *(first_arg); c != '\0'; c = *(++first_arg)) {
-        auto key = m.find(c);
-        if (key != m.end()) {
-            esc_string += m[c];
-        } else {
-            esc_string += c;
-        }
-    }
+
+
+//    for (std::string c = *(first_arg); c != '\0'; c = *(++first_arg)) {
+//        auto key = m.find(c);
+//        if (key != m.end()) {
+//            esc_string += m[c];
+//        } else {
+//            esc_string += c;
+//        }
+//    }
 
     std::cout << esc_string << std::endl;
 
     return EXIT_SUCCESS;
+}
+
+int num_utf8_continuation_bytes(std::string const& str) {
+
+    return str;
 }
--- scripts/mapmaker.js
+++ scripts/mapmaker.js
@@ -5,12 +5,12 @@ const { readFile } = require("fs").promises;
 readFile("escape_codes.json", "utf8").then(text => {
   const obj = JSON.parse(text);
 
-  console.log("static std::map<char, std::string> m {");
-  const esc_codes = Object.keys(obj);
+  console.log("static std::map<std::string, std::string> m {");
+  const esc_codes = Object.keys(obj).filter(key => key.endsWith(';'));
   esc_codes.forEach(key => {
     const props = obj[key];
     const characters = props.characters;
-    console.log(`    {'${characters}', "${key}"},`);
+    console.log(`    {"${characters}", "${key}"},`);
   });
   console.log("};");
 
