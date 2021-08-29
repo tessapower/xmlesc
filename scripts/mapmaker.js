3138b3e0aedf5df06d4848fad529125f3a8c9d75
Tessa Power <tessa@tessapower.co>
2021-08-29 16:32:09 +1200

    Finish creating cpp map of characters

--- scripts/mapmaker.js
+++ scripts/mapmaker.js
@@ -4,10 +4,14 @@ const { readFile } = require("fs").promises;
 
 readFile("escape_codes.json", "utf8").then(text => {
   const obj = JSON.parse(text);
+
+  console.log("static std::map<char, std::string> m {");
   const esc_codes = Object.keys(obj);
   esc_codes.forEach(key => {
     const props = obj[key];
     const characters = props.characters;
-    console.log(characters);
+    console.log(`    {'${characters}', "${key}"},`);
   });
+  console.log("};");
+
 });
