f2dd324e1186b9562cac66d41acdd001377d9943
Tessa Power <tessa@tessapower.co>
2021-08-29 15:01:18 +1200

    Parse json object

--- scripts/mapmaker.js
+++ scripts/mapmaker.js
@@ -2,7 +2,8 @@
 
 const { readFile } = require("fs").promises;
 
-readFile("escape_codes.json", "utf8")
-  .then(text => {
-    console.log(text)
-  });
+readFile("escape_codes.json", "utf8").then(text => {
+  const jsonObj = JSON.parse(text);
+  const englishHello = jsonObj.english;
+  console.log(englishHello);
+});
