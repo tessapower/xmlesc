231af97d5bbd0bfc4ac57719952e0d1fb5068e71
Tessa Power <tessa@tessapower.co>
2021-09-12 16:34:07 +1200

    Change map key from 'characters' to single 'codepoints'

--- scripts/mapmaker.js
+++ scripts/mapmaker.js
@@ -5,13 +5,22 @@ const { readFile } = require("fs").promises;
 readFile("escape_codes.json", "utf8").then(text => {
   const obj = JSON.parse(text);
 
-  console.log("static std::map<std::string, std::string> m {");
+  console.log("static std::map<uint32_t, std::string> m {");
+
+  // Some Unicode characters are made up of several codepoints, and can be
+  // interpreted with or without a semicolon. To avoid duplicates, and to
+  // keep this first version simple, we only want the unique entries that
+  // are made up of single codepoints.
+  //
   const esc_codes = Object.keys(obj).filter(key => key.endsWith(';'));
+
   esc_codes.forEach(key => {
     const props = obj[key];
-    const characters = props.characters;
-    console.log(`    {"${characters}", "${key}"},`);
+    const codepoints = props.codepoints;
+    if (codepoints.length === 1 ) {
+      console.log(`    {${codepoints}, "${key}"},`);
+    }
   });
-  console.log("};");
 
+  console.log("};");
 });
